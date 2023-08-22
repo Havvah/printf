3d7abc0bd66f89a5f7db2f5c19793dcf3be7360f
Udoka20 <136017484+Udoka20@users.noreply.github.com>
2023-08-22 13:57:48 +0100

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -3,12 +3,13 @@
 void print_buffer(char buffer[], int *buff_ind);
 
 /**
- * _printf - Printf function
- * @format: format.
- * Return: Printed chars.
+ * _printf - Printf function 
+ * @format: format. 
+ * Return: Printed chars. 
  */
 int _printf(const char *format, ...)
 {
+	
 	int i, printed = 0, printed_chars = 0;
 	int flags, width, precision, size, buff_ind = 0;
 	va_list list;
@@ -57,6 +58,7 @@ int _printf(const char *format, ...)
  * @buffer: Array of chars
  * @buff_ind: Index at which to add next char, represents the length.
  */
+
 void print_buffer(char buffer[], int *buff_ind)
 {
 	if (*buff_ind > 0)
